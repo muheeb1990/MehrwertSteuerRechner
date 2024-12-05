9479cf22407e22bc6c664150e5bb66546790796e
muheeb al sawaf <muheebalsawaf@gmail.com>
2024-12-05 15:17:45 +0100

    my first work on MehrwertSteuerRechner

---
+++ main.js
@@ -0,0 +1,32 @@
+/*let netto = document.getElementById('netto').value;
+let brutto = document.getElementById('brutto').value;
+
+/*let allgemeinSteuer= document.getElementById('satz-allgemein').value;
+
+let lebensmittelSteuer =document.getElementById('satz-ermäßigt').value;
+
+let steuerBetragRechner = function(){
+    if(allgemeinSteuer){
+    steuerBetrag = brutto - netto;
+    }
+    else{
+    steuerBetrag= netto *lebensmittelSteuer;
+    }
+    return(document.getElementById('betrag').value =steuerBetrag);
+}
+
+function checkRd(evt){
+    evt.preventDefault();
+    document.getElementById('betrag').value = 
+}
+
+
+function bruttoRechnen(netto){
+    if(allgemeinSteuer && netto != 0){
+    brutto = netto + (netto * allgemeinSteuer);}
+    else{
+    brutto = netto + (netto * lebensmittelSteuer);
+    }
+    return(brutto)
+}
+ */
